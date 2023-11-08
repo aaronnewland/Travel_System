dcba470f040919e193e797435f43d3ec743f2f93
Aaron Newland <51378352+aaronnewland@users.noreply.github.com>
2023-11-07 19:42:25 -0500

    Initial Commit

---
+++ src/main/java/com/example/travel_system/ApplicationDB.java
@@ -0,0 +1,116 @@
+package com.example.travel_system;
+
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+@WebServlet(name = "helloServlet", value = "/hello-servlet")
+public class ApplicationDB extends HttpServlet {
+
+    public ApplicationDB() {
+
+    }
+
+    public Connection getConnection(){
+
+        //Create a connection string
+        String connectionUrl = "jdbc:mysql://localhost:3306/Travel";
+        Connection connection = null;
+
+        try {
+            //Load JDBC driver - the interface standardizing the connection procedure. Look at WEB-INF\lib for a mysql connector jar file, otherwise it fails.
+            Class.forName("com.mysql.jdbc.Driver").newInstance();
+        } catch (InstantiationException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        try {
+            //Create a connection to your DB
+            connection = DriverManager.getConnection(connectionUrl,"root", "root");
+        } catch (SQLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        return connection;
+
+    }
+
+    public Connection getConnection(String connectionUrl){
+
+        //Create a connection string
+//        String connectionUrl = "jdbc:mysql://localhost:3306/Travel";
+        Connection connection = null;
+
+        try {
+            //Load JDBC driver - the interface standardizing the connection procedure. Look at WEB-INF\lib for a mysql connector jar file, otherwise it fails.
+            Class.forName("com.mysql.jdbc.Driver").newInstance();
+        } catch (InstantiationException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        try {
+            //Create a connection to your DB
+            connection = DriverManager.getConnection(connectionUrl,"root", "root");
+        } catch (SQLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        return connection;
+
+    }
+
+    public void closeConnection(Connection connection){
+        try {
+            connection.close();
+        } catch (SQLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+
+
+
+
+    public static void main(String[] args) {
+        ApplicationDB dao = new ApplicationDB();
+        Connection connection = dao.getConnection();
+
+        System.out.println(connection);
+        dao.closeConnection(connection);
+    }
+//    private String message;
+//
+//    public void init() {
+//        message = "Hello World!";
+//    }
+//
+//    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+//        response.setContentType("text/html");
+//
+//        // Hello
+//        PrintWriter out = response.getWriter();
+//        out.println("<html><body>");
+//        out.println("<h1>" + message + "</h1>");
+//        out.println("</body></html>");
+//    }
+//
+//    public void destroy() {
+//    }
+}
---
+++ src/main/webapp/checkLoginDetails.jsp
@@ -0,0 +1,34 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+         pageEncoding="ISO-8859-1" import="com.example.travel_system.*"%>
+<%@ page import="java.io.*,java.util.*,java.sql.*"%>
+<%@ page import="javax.servlet.http.*,javax.servlet.*"%>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<body>
+<%
+    String userid = request.getParameter("username");
+    String pwd = request.getParameter("password");
+    Class.forName("com.mysql.jdbc.Driver");
+    ApplicationDB db = new ApplicationDB();
+    Connection con = db.getConnection();
+//    Connection con = db.getConnection("jdbc:mysql://localhost:3306/Travel");
+//    Connection con = db.getConnection("jdbc:mysql://localhost:3306/Travel","root",
+//            "root");
+    Statement st = con.createStatement();
+    ResultSet rs;
+    rs = st.executeQuery("select * from login_info where username='" + userid + "' and password='" + pwd
+            + "'");
+    if (rs.next()) {
+        session.setAttribute("user", userid); // the username will be stored in the session
+        out.println("welcome " + userid);
+        out.println("<a href='logout.jsp'>Log out</a>");
+        response.sendRedirect("success.jsp");
+//          response.sendRedirect("displayLoginDetails.jsp");
+    } else {
+        out.println("Invalid password <a href='login.jsp'>try again</a>");
+    }
+%>
+</body>
+</html>
+
